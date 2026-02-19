448ca3cf1728cdaa577098fe6e90e79c9f80903f
Ralf Neumann <Ralf.Neumann@dci-student.org>
2026-02-19 14:11:40 +0100

    20260219

--- frontend/components/db.test.ts
+++ frontend/components/db.test.ts
@@ -7,7 +7,6 @@ console.log("POSTGRES_URL im Test:", process.env.POSTGRES_URL);
 describe("db connection", () => {
   it("should connect and execute a simple query", async () => {
     const result = await sql`SELECT 1 as value`;
-
     expect(result[0].value).toBe(1);
   });
 });
