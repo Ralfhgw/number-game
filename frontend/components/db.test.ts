a0b21f12072d91ffb1e197554059054727d07270
Ralf Neumann <Ralf.Neumann@dci-student.org>
2026-02-19 13:08:43 +0100

    20260219

--- frontend/components/db.test.ts
+++ frontend/components/db.test.ts
@@ -4,6 +4,7 @@ import sql from "./db";
 describe("db connection", () => {
   it("should connect and execute a simple query", async () => {
     const result = await sql`SELECT 1 as value`;
+    
     expect(result[0].value).toBe(1);
   });
 });
